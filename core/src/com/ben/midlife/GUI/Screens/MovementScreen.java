b04bb0751626697abd31c026b33f17a9b758f5d5
klkwarrior <ossusum.omnination@gmail.com>
2017-01-04 14:09:35 -0500

    Added Character Movement

--- core/src/com/ben/midlife/GUI/Screens/MovementScreen.java
+++ core/src/com/ben/midlife/GUI/Screens/MovementScreen.java
@@ -1,6 +1,8 @@
 package com.ben.midlife.GUI.Screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
@@ -9,10 +11,14 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.ben.midlife.Logic.Map;
+import com.ben.midlife.Logic.Player;
 import com.ben.midlife.Logic.PlayerBody;
 import com.ben.midlife.Logic.Tile;
 import com.ben.midlife.MidLife;
+import com.sun.prism.image.ViewPort;
 
 import java.util.Random;
 
@@ -26,20 +32,82 @@ public class MovementScreen implements Screen {
     private OrthographicCamera camera;
     private Box2DDebugRenderer debugRenderer;
     private PlayerBody playerBody;
+    private ExtendViewport viewPort;
+    private Stage stage;
 
     public MovementScreen(MidLife midLife) {
         this.midLife = midLife;
-        this.world = new World(new Vector2(0, -10), true);
+        this.world = new World(new Vector2(0, 0), true);
         // create the camera and the SpriteBatch
         this.camera = new OrthographicCamera();
-
         this.map = new Map(this.world, new Random().nextInt(10)+10,new Random().nextInt(10)+10);
         this.playerBody = new PlayerBody(this.world);
-        this.camera.setToOrtho(false, 10, 10);
+        this.camera.setToOrtho(false, 10,10);
+        //Gdx.app.getGraphics().setWindowedMode(1000,400);
         //this.camera.translate(-camera.viewportWidth / 2, -camera.viewportHeight / 2, 0);
         map.generateRandomMap();
         this.debugRenderer = new Box2DDebugRenderer();
 
+        Gdx.input.setInputProcessor(new InputProcessor() {
+            @Override
+            public boolean keyDown(int keycode) {
+                if (keycode == Input.Keys.DOWN)
+                playerBody.getPlayerBody().applyLinearImpulse(new Vector2(0,-2), new Vector2(0,-2), true);
+                if (keycode == Input.Keys.UP)
+                    playerBody.getPlayerBody().applyLinearImpulse(new Vector2(0,2), new Vector2(0,2), true);
+                if (keycode == Input.Keys.LEFT)
+                    playerBody.getPlayerBody().applyLinearImpulse(new Vector2(-2,0), new Vector2(-2,0), true);
+                if (keycode == Input.Keys.RIGHT)
+                    playerBody.getPlayerBody().applyLinearImpulse(new Vector2(2,0), new Vector2(2,0), true);
+
+                return true;
+            }
+
+            @Override
+            public boolean keyUp(int keycode) {
+                playerBody.getPlayerBody().setLinearVelocity(new Vector2(0,0));
+                return true;
+            }
+
+            @Override
+            public boolean keyTyped(char character) {
+                if (character == 's')
+                    playerBody.getPlayerBody().applyLinearImpulse(new Vector2(0,-2), new Vector2(0,-2), true);
+                if (character == 'w')
+                    playerBody.getPlayerBody().applyLinearImpulse(new Vector2(0,2), new Vector2(0,2), true);
+                if (character == 'a')
+                    playerBody.getPlayerBody().applyLinearImpulse(new Vector2(-2,0), new Vector2(-2,0), true);
+                if (character == 'd')
+                    playerBody.getPlayerBody().applyLinearImpulse(new Vector2(2,0), new Vector2(2,0), true);
+
+                return true;
+            }
+
+            @Override
+            public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+                return false;
+            }
+
+            @Override
+            public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+                return false;
+            }
+
+            @Override
+            public boolean touchDragged(int screenX, int screenY, int pointer) {
+                return false;
+            }
+
+            @Override
+            public boolean mouseMoved(int screenX, int screenY) {
+                return false;
+            }
+
+            @Override
+            public boolean scrolled(int amount) {
+                return false;
+            }
+        });
     }
 
     @Override
@@ -49,9 +117,10 @@ public class MovementScreen implements Screen {
 
     @Override
     public void render(float delta) {
-        this.camera.position.set(playerBody.getPosition(), 0);
-        camera.update();
         world.step(1 / 60f, 6, 2);
+        camera.position.set(playerBody.getPlayerBody().getPosition(), 0);
+        camera.update();
+
 
         // clear the screen with a dark blue color. The
         // arguments to glClearColor are the red, green
--- desktop/src/com/ben/midlife/desktop/DesktopLauncher.java
+++ desktop/src/com/ben/midlife/desktop/DesktopLauncher.java
@@ -7,6 +7,7 @@ import com.ben.midlife.MidLife;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
 		new LwjglApplication(new MidLife(), config);
 	}
 }
